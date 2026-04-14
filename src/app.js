ee0e395ac9482d9149dd3be8762ff255e7991332
Pratham Sharma <prathamsharma96@example.com>
2026-04-14 11:21:41 +0530

    implement route handlers

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
 
-app.use("/helllo", (req, res) => {
-  res.send("Hello World");
+app.delete("/hello", (req, res) => {
+  res.send("data updated successfully");
 });
+app.put("/hello", (req, res) => {
+  res.send("put request received");
+});
+app.use("/user", (req, res) => {
+  res.send(req.query);
+});
+
+//this is a new route for saying goodbye it will run even when route is /goodbyehello
+// app.use("/goodbye", (req, res) => {
+//   res.send("Goodbye World");
+// });
+//  app.use("/", (req, res) => {
+//   res.send("thils is the default route");
+// });
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
